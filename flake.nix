b1e182329555c2194bc1fd91478387a5e53b6e5d
Martin Toman <toman.martin@live.com>
2022-12-02 17:45:19 -0800

    Nix flake update

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-22.11;
     flake-utils.url = github:numtide/flake-utils/master;
     devshell = {
       url = "github:numtide/devshell/master";
